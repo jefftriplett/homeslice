b513a2b497481c201dc1592c330312c732e85593
Jeff Triplett <jeff.triplett@gmail.com>
2017-07-04 01:54:59 -0500

    Updates setup

--- setup.py
+++ setup.py
@@ -1,14 +1,45 @@
+import codecs
+import os
+import sys
 
 from setuptools import setup, find_packages
 
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(here, 'homeslice', '__version__.py')) as f:
+    exec(f.read(), about)
+
+with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+
+required = [
+    'click',
+    'click_completion',
+    'crayons',
+]
+
+if sys.version_info < (2, 7):
+    required.append('pathlib')
+
+
 setup(
     name='homeslice',
-    version='0.1.0',
+    version=about['__version__'],
     description='Dotfile management and synchronisation tool',
-    long_description=open('README.rst').read(),
-    url='https://github.com/acroz/pyhome',
-    author='Andrew Crozier',
-    author_email='wacrozier@gmail.com',
+    long_description=long_description,
+    url='https://github.com/jefftriplett/homeslice',
+    author='Jeff Triplett',
+    author_email='jeff.triplett@gmail.com',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'homeslice=homeslice:cli'
+        ]
+    },
+    install_requires=required,
     license='MIT',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -25,10 +56,4 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5'
     ],
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-          'homeslice=homeslice:cli'
-        ]
-    }
 )
